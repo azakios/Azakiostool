dc057997b7213dd3b49033c8cfbc9baee08eb669
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-11 14:19:22 +0530

    Add files via upload

---
+++ t2.sh
@@ -0,0 +1,23 @@
+read -p 'Enter Your Name :' userinput1;
+#
+if [ "${userinput1:-}" = "Azakios" ]
+then
+	echo -e "\e[93mNice To See you Sir\e[0m";
+fi
+read -p 'How Are You Sir? :' userinput2;
+#
+if [ "${userinput2:-}" = "Fine" ]
+then
+	echo -e "\e[35mGreat stay happy\e[0m";
+fi
+if [ "${userinput2:-}" = "Anu" ]
+then
+	echo -e "\e[94mHey Queen wow nice to see u here\e[0m";
+fi
+read -p 'Are U fine Dear Queen :' userinput3;
+#
+if [ "${userinput3:-}" = "Yeah" ]
+then
+	echo -e "\e[32mOsm STAY ALWAYS SMILING AND HAPPY ,NO MOODY FACE HEHEHE HUHUHUHU\e[0m";
+fi
+figlet  Thanks For Taking Your Precious Time
