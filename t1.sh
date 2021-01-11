74489ee8c45b88c86c0649d6a5653fd80fba14d3
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-11 13:53:45 +0530

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,21 +1,58 @@
-pkg  install figlet && figlet  Azakios
+pkg  install figlet && clear && figlet  Azakios
 echo -e "\e[36mWELCOME EVERYONE\e[0m";
 echo -e "\e[34m(+)TOOL BY AZAKIOS TEAM\e[0m";
-read -p 'ENTER YOUR NAME OR NAME OF ANY MEMBER OF AZAKIOS TEAM ' userinput1;
+read -p 'ENTER YOUR NAME OR NAME' userinput1;
 #
-if [ "${userinput1:-}" = "Azakios || Anjuman || Mushahid" ]
+if [ "${userinput1:-}" = "Azakios" ]
 then
 	echo -e "\e[93mAS SALAMU ALYKUM Dear \e[0m";
+
+if [ "${userinput1:-}" = "Anu" ]
+then
+	echo -e "\e[93mAS SALAMU ALYKUM Dear Queen \e[0m";
+
+if [ "${userinput1:-}" = "Mushahid" ]
+then
+	echo -e "\e[93mAS SALAMU ALYKUM Dear \e[0m";
+
 fi
 read -p 'HOW ARE YOU? ' userinput2;
 #
-if [ "${userinput2:-}" = "Fine || Good || Feeling good" ]
+if [ "${userinput2:-}" = "Fine" ]
 then
 	echo -e "\e[94mNICE TO HEAR THAT MY DUAS ALWAYS FOR U\e[0m";
+
+if [ "${userinput2:-}" = "Good" ]
+then
+	echo -e "\e[94mNICE TO HEAR THAT MY DUAS ALWAYS FOR U\e[0m";
+
+if [ "${userinput2:-}" = "Feeling good" ]
+then
+	echo -e "\e[94mNICE TO HEAR THAT MY DUAS ALWAYS FOR U\e[0m";
+
 fi
-if [ "${userinput2:-}" = "Sick || ill || bored || not feeling well || sad " ]
+
+
+if [ "${userinput2:-}" = "Sick" ]
 then
 	figlet  SMILE EVERYTHING WILL BE FINE
+
+
+if [ "${userinput2:-}" = "ill" ]
+then
+	figlet  SMILE EVERYTHING WILL BE FINE
+if [ "${userinput2:-}" = "bored" ]
+then
+	figlet  SMILE EVERYTHING WILL BE FINE,Padd sabakh
+
+if [ "${userinput2:-}" = "not feeling well" ]
+then
+	figlet  SMILE EVERYTHING WILL BE FINE,Khuda sub krie sahlie
+
+if [ "${userinput2:-}" = "sad " ]
+then
+	figlet  Oii SMILE EVERYTHING WILL BE FINE,Tnsn mae raeth
+
 fi
 echo -e "\e[33mBE PATIENT \e[0m";
 read -p 'Did u enjoy this (yes or no)' userinput3;
