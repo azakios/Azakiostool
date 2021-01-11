6cedb53300b7f68904d180be8453ca3709d4c4d4
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-11 13:34:52 +0530

    Add files via upload

---
+++ t1.sh
@@ -0,0 +1,33 @@
+pkg  install figlet
+exit  0 && ls && bash t1.sh
+figlet  Azakios
+echo -e "\e[36mWELCOME EVERYONE\e[0m";
+echo -e "\e[34m(+)TOOL BY AZAKIOS TEAM\e[0m";
+read -p 'ENTER YOUR NAME OR NAME OF ANY MEMBER OF AZAKIOS TEAM ' userinput1;
+#
+if [ "${userinput1:-}" = "Azakios || Anjuman || Mushahid" ]
+then
+	echo -e "\e[93mAS SALAMU ALYKUM Dear \e[0m";
+fi
+read -p 'HOW ARE YOU? ' userinput2;
+#
+if [ "${userinput2:-}" = "Fine || Good || Feeling good" ]
+then
+	echo -e "\e[94mNICE TO HEAR THAT MY DUAS ALWAYS FOR U\e[0m";
+fi
+if [ "${userinput2:-}" = "Sick || ill || bored || not feeling well || sad " ]
+then
+	figlet  SMILE EVERYTHING WILL BE FINE
+fi
+echo -e "\e[33mBE PATIENT \e[0m";
+read -p 'Did u enjoy this (yes or no)' userinput3;
+#
+if [ "${userinput3:-}" = "yes" ]
+then
+	figlet  Thank You Dear
+fi
+if [ "${userinput3:-}" != "exit 1" ]
+then
+	ls  bash t1.sh
+;
+fi
