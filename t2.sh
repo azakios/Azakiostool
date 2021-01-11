68d57009727b196a4efff4b0947862a430b7981b
Azakios <70518167+azakios@users.noreply.github.com>
2021-01-11 14:26:20 +0530

    Update t2.sh

--- t2.sh
+++ t2.sh
@@ -6,18 +6,17 @@ read -p 'Enter Your Name :' userinput1;
 if [ "${userinput1:-}" = "Azakios" ]
 then
 	echo -e "\e[93mNice To See you Sir\e[0m";
+elif [ "${userinput1:-}" = "Anu" ]
+then
+	echo -e "\e[94mHey Queen wow nice to see u here\e[0m";
 fi
-read -p 'How Are You Sir? :' userinput2;
+read -p 'How Are You? :' userinput2;
 #
 if [ "${userinput2:-}" = "Fine" ]
 then
 	echo -e "\e[35mGreat stay happy\e[0m";
 fi
-if [ "${userinput1:-}" = "Anu" ]
-then
-	echo -e "\e[94mHey Queen wow nice to see u here\e[0m";
-fi
-read -p 'Are U fine Dear Queen :' userinput3;
+read -p 'Are U fine Dear :' userinput3;
 #
 if [ "${userinput3:-}" = "Yeah" ]
 then
